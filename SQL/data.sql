81ad8fe2b5a22bc3d5ce51f580e3fb36242a6b3f
Jessica Prieto <jessicaprieto31@gmail.com>
2018-02-12 15:43:29 -0700

    update

---
+++ SQL/data.sql
@@ -0,0 +1,16 @@
+CREATE table de.user (
+    userID      int,
+    username    VARCHAR(40),
+    password    VARCHAR(40)
+);
+
+CREATE table de.laptop (
+    part        VARCHAR(30),
+    price       int
+);
+
+CREATE table de.desktop (
+    partname    text,
+    shortname   VARCHAR(30),
+    price       int
+);
